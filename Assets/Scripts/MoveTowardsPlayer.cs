3b8140cad208b1443dd57138cda14febae58c54d
Janikowski, Josh <janikows@fvtc.edu>
2019-07-12 06:58:50 -0500

    move towards player edited

--- Assets/Scripts/MoveTowardsPlayer.cs
+++ Assets/Scripts/MoveTowardsPlayer.cs
@@ -2,28 +2,46 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MoveTowardsPlayer : MonoBehaviour {
+public class MoveTowardsPlayer : MonoBehaviour
+{
+    Animator animator;
 
- //The target player
- public Transform player;
- //At what distance will the enemy walk towards the player?
- public float walkingDistance = 10.0f;
- //In what time will the enemy complete the journey between its position and the players position
- public float smoothTime = 10.0f;
- //Vector3 used to store the velocity of the enemy
- private Vector3 smoothVelocity = Vector3.zero;
- //Call every frame
- void Update()
- {
-     //Look at the player
-     transform.LookAt(player);
-     //Calculate distance between player
-     float distance = Vector3.Distance(transform.position, player.position);
-     //If the distance is smaller than the walkingDistance
-     if(distance < walkingDistance)
-     {
-         //Move the enemy towards the player with smoothdamp
-         transform.position = Vector3.SmoothDamp(transform.position, player.position, ref smoothVelocity, smoothTime);
-     }
- }
+    //The target player
+    public Transform player;
+    //At what distance will the enemy walk towards the player?
+    public float walkingDistance = 20.0f;
+    public float attackdistance = 4.0f;
+    //In what time will the enemy complete the journey between its position and the players position
+    public float smoothTime = 10.0f;
+    //Vector3 used to store the velocity of the enemy
+    private Vector3 smoothVelocity = Vector3.zero;
+
+    public void Start()
+    {
+        animator = gameObject.GetComponent<Animator>();
+    }
+
+
+    //Call every frame
+    void Update()
+    {
+        //Look at the player
+        transform.LookAt(player);
+        //Calculate distance between player
+        float distance = Vector3.Distance(transform.position, player.position);
+        //If the distance is smaller than the walkingDistance
+        if (distance < walkingDistance)
+        {
+            //Move the enemy towards the player with smoothdamp
+            transform.position = Vector3.SmoothDamp(transform.position, player.position, ref smoothVelocity, smoothTime);
+
+
+        }
+        else if (distance < attackdistance)
+        {
+            animator.SetBool("ZombieisClose", true);
+        }
+        
+
+    }
 }
